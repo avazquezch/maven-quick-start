f4d5e5db65cbbce711ad1f02d6c0abf6b97ac739
Alejandro Vázquez Chávez <alejandro.vazquez.chavez@bbvaitspain.com>
2023-01-28 00:49:47 +0100

    Adding Application Java source

---
+++ src/main/java/avazquezch/training/Aplication.java
@@ -0,0 +1,14 @@
+package avazquezch.training;
+
+public class Aplication {
+    
+    public Aplication() {
+        System.out.println ("Inside Aplication");
+    }
+
+    // method main(): ALWAYS the Aplication entry point
+    public static void main (String[] args) {
+    	System.out.println ("Starting Aplication");
+	Aplication app = new Aplication();
+    }
+}

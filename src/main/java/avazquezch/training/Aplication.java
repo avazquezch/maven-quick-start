324bebb44d8bf2c9b789fc14f0d91ede4a516242
Alejandro Vázquez Chávez <alejandro.vazquez.chavez@bbvaitspain.com>
2023-01-28 09:23:48 +0100

    Changing compiler plugin to Java source 1.8

--- src/main/java/avazquezch/training/Aplication.java
+++ src/main/java/avazquezch/training/Aplication.java
@@ -1,6 +1,18 @@
 package avazquezch.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aplication {
+
+    public void greet() {
+        List<String> greetings = new ArrayList<>();
+        greetings.add("Hello");
+
+        for (String greeting  : greetings) {
+            System.out.println("Greeting: " + greeting);
+        }
+    }
     
     public Aplication() {
         System.out.println ("Inside Aplication");
@@ -10,5 +22,6 @@ public class Aplication {
     public static void main (String[] args) {
     	System.out.println ("Starting Aplication");
 	Aplication app = new Aplication();
+    app.greet();
     }
 }
